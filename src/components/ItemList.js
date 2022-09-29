623ff49cfbe21a889621868222f6d9c6cd786aed
ceimiplace <100032730+ceimiplace@users.noreply.github.com>
2022-09-29 22:47:51 +0300

    Update ItemList.js

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,21 +1,16 @@
 import React from "react";
-class ItemsList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className="columns-2xs xl:columns-sm">
-        {this.props.images.map((image) => (
-          <img
-            src={image.urls.regular}
-            key={image.id}
-            className="m-2"
-            alt="image"
-          />
-        ))}
-      </div>
-    );
-  }
+function ItemsList({ images }) {
+  return (
+    <div className="columns-2xs xl:columns-sm">
+      {images.map((image) => (
+        <img
+          src={image.urls.regular}
+          key={image.id}
+          className="m-2"
+          alt="image"
+        />
+      ))}
+    </div>
+  );
 }
 export default ItemsList;
