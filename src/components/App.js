2907ffc0aad3313d191fef685b43cdc3bdb65676
ceimiplace <100032730+ceimiplace@users.noreply.github.com>
2022-09-29 23:04:17 +0300

    refractor hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import SearchBar from "./SearchBar";
 import ItemsList from "./ItemList";
-class App extends React.Component {
-  state = { images: [] };
-  whenUserSubmitsForm = async (term) => {
-    console.log(term);
-    const response = await axios.get(
-      "https://api.unsplash.com/search/photos/",
-      {
-        params: { query: term },
-        headers: {
-          Authorization:
-            "Client-ID TYMtIDr5pbkUTBDnWOfebqltBPGuNdvPoUVcZSvCqJA",
-        },
-      }
-    );
+function App() {
+  const [images, setImages] = useState([]);
+  const [term, setTerm] = useState("dogs");
 
-    this.setState({ images: response.data.results });
-  };
-  render() {
-    return (
-      <div className="App w-4/6 flex  flex-col  p-3  mx-auto">
-        <SearchBar submited={this.whenUserSubmitsForm} />
-        <ItemsList images={this.state.images} />
-      </div>
-    );
+  useEffect(() => {
+    fetch(`https://api.unsplash.com/search/photos/${term}`, {
+      method: "GET",
+      headers: {
+        Authorization: "Client-ID TYMtIDr5pbkUTBDnWOfebqltBPGuNdvPoUVcZSvCqJA",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => setImages(data.results));
+  }, [term]);
+
+  function whenUserSubmitsForm(term) {
+    setTerm(term);
   }
+  // whenUserSubmitsForm = async (term) => {
+  //   console.log(term);
+  //   const response = await axios.get(
+  //     "https://api.unsplash.com/search/photos/",
+  //     {
+  //       params: { query: term },
+  //       headers: {
+  //         Authorization:
+  //           "Client-ID TYMtIDr5pbkUTBDnWOfebqltBPGuNdvPoUVcZSvCqJA",
+  //       },
+  //     }
+  //   );
+
+  //    );
+  // };
+
+  return (
+    <div className="App w-4/6 flex  flex-col  p-3  mx-auto">
+      <SearchBar submited={whenUserSubmitsForm} />
+      <ItemsList images={images} />
+    </div>
+  );
 }
 
 export default App;
